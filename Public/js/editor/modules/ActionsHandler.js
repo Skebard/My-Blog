238b9cffe3a94b1aca839290000e28a0b4477904
Toni <49041487+Skebard@users.noreply.github.com>
2020-11-04 15:54:44 +0300

    folder reestructure

--- Public/js/editor/modules/ActionsHandler.js
+++ Public/js/editor/modules/ActionsHandler.js
@@ -1,5 +1,25 @@
-export default ActionsHandler{
-    
+export default class ActionsHandler{
+    //this class is in charge for saving, publishing and cancel
 
+    //Sent all the data to server and get response
+    save(){
+        //data should be saved in the database, but without creating any file
+
+    }
+
+    //sent all the data to server and get response
+    publish(){
+        //when publishing a post, a .php file with the name of the post will be created
+
+    }
+
+    withdaw(){
+
+    }
+
+    //go back to the main page
+    cancel(){
+
+    }
 
 }
